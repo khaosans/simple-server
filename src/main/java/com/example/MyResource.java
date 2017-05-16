bb0ebfd43194d096c0fe4c86b41fc6ee8f9d2779
Souriya Khaosanga <skhaosanga@jamasoftware.com>
2017-05-16 16:35:27 -0700

    fully functioning cache with put and get

--- src/main/java/com/example/MyResource.java
+++ src/main/java/com/example/MyResource.java
@@ -10,7 +10,6 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.conn.HttpHostConnectException;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 
 import javax.inject.Singleton;
 import javax.ws.rs.*;
@@ -55,10 +54,10 @@ public class MyResource {
             return Response.status(404).entity("Item does not exist").build();
         }
 
-        String responseAsString = EntityUtils.toString(httpResponse.getEntity());
+        int statusCodeOfResponse = httpResponse.getStatusLine().getStatusCode();
 
         return Response.status(200)
-                .entity(responseAsString).lastModified(lastModified).cacheControl(cc)
+                .entity(statusCodeOfResponse).lastModified(lastModified).cacheControl(cc)
                 .build();
     }
 
--- src/test/java/com/example/MyResourceTest.java
+++ src/test/java/com/example/MyResourceTest.java
@@ -14,6 +14,8 @@ import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MultivaluedHashMap;
 
+import java.util.Objects;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -72,7 +74,8 @@ public class MyResourceTest {
         form.add("url", "http://www.facebook.com");
         String faceBookHex = target.path("cache").request().post(Entity.form(form), String.class);
         assertTrue(faceBookHex.equals("fcdaa82b65917e050dcb45f818af3382f8c0a961"));
-        assertTrue(is200Response(form));    }
+        assertTrue(is200Response(form));
+    }
 
     @Test
     public void testYahooHex() throws Exception {
@@ -80,7 +83,8 @@ public class MyResourceTest {
         form.add("url", "http://www.yahoo.com");
         String yahooHex = target.path("cache").request().post(Entity.form(form), String.class);
         assertTrue(yahooHex.equals("11897a5a38e50c6b72c7588c8ed6a153d866d5c0"));
-        assertTrue(is200Response(form));    }
+        assertTrue(is200Response(form));
+    }
 
     @Test(expected = BadRequestException.class)
     public void testInvalidUrl() throws Exception {
@@ -94,6 +98,16 @@ public class MyResourceTest {
     public void testBadShaGet() throws Exception {
         String response = target.path("cache/123").request().get(String.class);
         assertTrue(response.equals("Item does not exist"));
+    }
+
+    @Test
+    public void testPutAndGet() throws Exception {
+        MultivaluedHashMap<String, String> form = new MultivaluedStringMap();
+        form.add("url", "http://www.google.com");
+        String hex = target.path("cache").request().post(Entity.form(form), String.class);
+        assertTrue(hex.equals("738ddf35b3a85a7a6ba7b232bd3d5f1e4d284ad1"));
+        String response = target.path("cache/738ddf35b3a85a7a6ba7b232bd3d5f1e4d284ad1").request().get(String.class);
+        assert (Objects.equals(response, "200"));
     }
 
     private Boolean is200Response(MultivaluedHashMap<String, String> form) {
